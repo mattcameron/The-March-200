c68b7ff95491f91fd49f50e52b0d48aa7def1865
Matt Cameron <mcameron@avalanche.com.au>
2016-03-06 17:32:19 +1100

    Add jquery turbolinks. Style index

--- app/helpers/runs_helper.rb
+++ app/helpers/runs_helper.rb
@@ -1,2 +1,13 @@
 module RunsHelper
+
+  def run_total(attr)
+    Run.sum(attr.to_sym)
+  end
+
+  def minutes_per_km(speed)
+    seconds = (speed % 60).floor
+    minutes = ((speed / 60) % 60).floor
+
+    "#{minutes}:#{seconds} per km"
+  end
 end
--- app/models/run.rb
+++ app/models/run.rb
@@ -1,2 +1,9 @@
 class Run < ActiveRecord::Base
+  before_save :calc_speed
+
+  private 
+
+  def calc_speed
+    self.speed = (time / distance)
+  end
 end
